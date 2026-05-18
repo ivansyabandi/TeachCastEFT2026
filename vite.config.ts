2c0048ca4a1a333ce3b802bac024bfe9ba5421b9
Ivan Anwar Syabandi <ivansyabandi@outlook.com>
2026-05-18 18:38:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/EFT-By-TeachCast-x-GIKUGM-2026/',
+    base: '/TeachCastEFT2026/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
