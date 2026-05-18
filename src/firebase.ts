b8be5788d91e7cc3fe4963102cb79ad1e33c5c1e
Ivan Anwar Syabandi <ivansyabandi@outlook.com>
2026-05-18 18:26:52 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASOL5YqYWvhKVdD6aTYsYifl9oljIO_Zg",
-  authDomain: "eft-redeem-code.firebaseapp.com",
-  databaseURL: "https://eft-redeem-code-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "eft-redeem-code",
-  storageBucket: "eft-redeem-code.firebasestorage.app",
-  messagingSenderId: "98805584959",
-  appId: "1:98805584959:web:c27cfd89a2657d8d53148a"
+  apiKey: "AIzaSyDKCCYzvCKWD5Z7PughlUMd552_YZDyDFE",
+  authDomain: "eft-uin-suka.firebaseapp.com",
+  databaseURL: "https://eft-uin-suka-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "eft-uin-suka",
+  storageBucket: "eft-uin-suka.firebasestorage.app",
+  messagingSenderId: "232770363304",
+  appId: "1:232770363304:web:fee223bcaf7542aa2c879d",
 };
 
 const app = initializeApp(firebaseConfig);
